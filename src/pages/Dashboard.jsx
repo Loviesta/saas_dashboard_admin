eb4295e1961cf983d1531b921c231196e6caf3ab
Loviesta <loviestauye123@gmail.com>
2026-07-05 20:41:33 +0700

    fix: force change file case for deployment

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import RevenueChart from "../components/charts/RevenueChart";
 import ActivityList from "../components/activity/ActivityList";
 import TargetAnalysis from "../components/cards/TargetAnalysis";
 
-import { stats } from "../data/dashboardData";
+import { stats } from "../data/DashboardData";
 
 export default function Dashboard() {
   return (
